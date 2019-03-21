ea096e664f700d569acd3a27e4f20e1b83287de3
root <ratnadeep.konar@gmail.com>
2019-03-21 02:16:15 +0000

    initial commit

---
+++ src/Score.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+  const Score = props => {
+  return (
+    <p className="text">
+      Your Score: {props.numCorrect}/{props.numQuestions}
+    </p>
+  );
+};
+
+export default Score;
